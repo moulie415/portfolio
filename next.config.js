0dd4dc828d271c7e0975364b5cf7e07ff88bba5d
Henry Moule <hen10@hotmail.co.uk>
2023-03-01 22:11:29 +0000

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
